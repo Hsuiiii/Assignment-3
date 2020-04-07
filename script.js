171735b62c5746410c3eba077d086980bfad2531
Hsuiiii <60276239+Hsuiiii@users.noreply.github.com>
2020-04-07 19:00:56 -0400

    changes

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ $('.emoji').click(function(){
   var text = $('textarea').val();
   $('textarea').val(text+emoji)
 });
+
+function myFunction() {
+  var copyText = document.getElementById("imput");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  alert("Copied the text: " + copyText.value);
+}
